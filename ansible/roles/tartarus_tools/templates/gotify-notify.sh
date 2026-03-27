22a35d9290465e40eecdb4a11b4be6440615f618
Benjamin Becker <18665277+blbecker@users.noreply.github.com>
2026-03-26 22:07:57 -0400

    chore: ansible-lint

--- ansible/roles/tartarus_tools/templates/gotify-notify.sh
+++ ansible/roles/tartarus_tools/templates/gotify-notify.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-set -eu 
+set -eu
 
-url="{{ gotify_url }}"
+url="{{ tartarus_tools_gotify_url }}"
 
 app_token="$1"
 title="$2"
 message="$3"
 
 curl -X POST "https://${url}/message?token=${app_token}" -F "title=${title}" -F "message=${message}"s
+
